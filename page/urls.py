c143255a9219d59b4d711c9a6abbf4faeb8dad8e
tadejpetric <tadej.petric1@gmail.com>
2019-05-11 17:52:36 +0200

    added racuni app which will be used for UVP project

--- page/urls.py
+++ page/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('analiza/', include('page.analiza.urls')),
+    path('racuni/', include('page.racuni.urls')),
 ]
